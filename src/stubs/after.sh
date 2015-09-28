3b64db068bf9ae302d502de4e620dcc27638eae9
Wouter Bulten <wouterbulten@mai-ai.org>
2015-09-28 15:16:51 +0200

    Added better install of npm packages

--- src/stubs/after.sh
+++ src/stubs/after.sh
@@ -74,7 +74,18 @@ EOL
 
 # PDF Output
 cd
-mkdir tmp
+mkdir -p tmp
 cd tmp
 wget http://download.gna.org/wkhtmltopdf/0.12/0.12.2.1/wkhtmltox-0.12.2.1_linux-trusty-amd64.deb
 sudo dpkg -i wkhtmltox-0.12.2.1_linux-trusty-amd64.deb
+
+# Fix NPM location
+mkdir -p /vagrant/npm-global
+npm config set prefix '/vagrant/npm-global'
+
+# Install global packages
+npm install -g mocha
+
+# Install npm packages
+cd /home/vagrant/testlokaal/testing
+npm install
